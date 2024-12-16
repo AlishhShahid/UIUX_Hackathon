67d7d15d9df64c55e010750d7b1060ebb31c432c
Alishba_Shahid <165990022+AlishhShahid@users.noreply.github.com>
2024-12-17 01:32:02 +0500

    Update page.tsx

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,14 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
 import SideBar from "../components/SideBar";
 import Third02 from "../components/Third02";
 import Cars from "../components/Cars";
@@ -16,19 +5,13 @@ import Cars from "../components/Cars";
 function Category() {
   return (
     <div className="flex flex-col md:flex-row min-h-screen">
-      {/* Sidebar */}
       <div className="w-full md:w-1/4 bg-gray-100 border-r border-gray-200">
         <SideBar />
       </div>
-
-      {/* Main Content */}
-      <div className="w-full md:w-3/4 p-4">
-        {/* Top Search Bar */}
+      <div className="md:w-3/4 p-1">
         <div className="mb-4">
           <Third02 />
         </div>
-
-        {/* Cars Section */}
         <div className="">
           <Cars />
         </div>
@@ -38,26 +21,3 @@ function Category() {
 }
 
 export default Category;
-
-
-
-
-
-// import SideBar from "../components/SideBar"
-// import Third from "../components/Third"
-// import Cars from "../components/Cars"
-
-
-
-// function Category(){
-//     return(
-//       <>
-//       <SideBar/>
-//       <Third/>
-//       <Cars/>
-      
-//       </>
-//     )
-// }
-
-// export default Category
