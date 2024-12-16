d64460560a26d73ef70e25cdde4b063cda20856e
Alishba_Shahid <165990022+AlishhShahid@users.noreply.github.com>
2024-12-17 01:35:42 +0500

    Update Third02.tsx

--- app/components/Third02.tsx
+++ app/components/Third02.tsx
@@ -3,14 +3,14 @@ import DropoffSection from "./DropoffSection";
 
 function Third02() {
   return (
-    <div className="flex items-center justify-between px-6 py-5 bg-white rounded-lg shadow-md w-full max-w-7xl mx-auto">
+    <div className="flex flex-col sm:flex-row items-center justify-between px-2 py-5 sm:mr-44 mr-96">
 
       <div className="flex-shrink-0 w-1/3">
         <PickupSection />
       </div>
 
       <div className="flex items-center justify-center ml-44 ">
-        <img src="/Switch.png" alt="switch icon" className="" />
+        <img src="/Switch.png" alt="switch icon" className="w-40 h-20" />
       </div>
 
       <div className="flex-shrink-0 w-1/3">
@@ -21,7 +21,3 @@ function Third02() {
 }
 
 export default Third02;
-
-
-
-
