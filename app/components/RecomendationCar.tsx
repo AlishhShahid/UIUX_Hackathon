56e4e8846785796eaf1f025bf6e14a9ba49c2fe0
Alishba_Shahid <165990022+AlishhShahid@users.noreply.github.com>
2025-01-11 20:14:44 +0500

    Update RecomendationCar.tsx

--- app/components/RecomendationCar.tsx
+++ app/components/RecomendationCar.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import CarRentalCard from "./CarRentalCard";
+
 function RecomendationCar() {
   return (
     <div className="px-8 py-8 bg-white relative">
@@ -6,68 +9,85 @@ function RecomendationCar() {
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-4 gap-x-6 gap-y-8 mb-12">
-        <div>
-          <img
-            src="/1.png"
-            alt="01"
-            className="w-full h-auto rounded-lg shadow-sm"
-          />
-        </div>
-        <div>
-          <img
-            src="/2.png"
-            alt="02"
-            className="w-full h-auto rounded-lg shadow-sm"
-          />
-        </div>
-        <div>
-          <img
-            src="/3.png"
-            alt="03"
-            className="w-full h-auto rounded-lg shadow-sm"
-          />
-        </div>
-        <div>
-          <img
-            src="/4.png"
-            alt="04"
-            className="w-full h-auto rounded-lg shadow-sm"
-          />
-        </div>
-        <div>
-          <img
-            src="/5.png"
-            alt="05"
-            className="w-full h-auto rounded-lg shadow-sm"
-          />
-        </div>
-        <div>
-          <img
-            src="/6.png"
-            alt="06"
-            className="w-full h-auto rounded-lg shadow-sm"
-          />
-        </div>
-        <div>
-          <img
-            src="/7.png"
-            alt="07"
-            className="w-full h-auto rounded-lg shadow-sm"
-          />
-        </div>
-        <div>
-          <img
-            src="/8.png"
-            alt="08"
-            className="w-full h-auto rounded-lg shadow-sm"
-          />
-        </div>
+        <CarRentalCard
+          name="All New Rush"
+          type="SUV"
+          image1="/05.png"
+          image2="/Spesification4.png"
+          price="$72.00"
+          cutPrice="$80.00"
+        />
+
+        <CarRentalCard
+          name="CR  - V"
+          type="SUV"
+          image1="/06.png"
+          image2="/Spesification5.png"
+          price="$80.00"
+          cutPrice=" "
+        />
+
+        <CarRentalCard
+          name="All New Terios"
+          type="SUV"
+          image1="/07.png"
+          image2="/Spesification6.png"
+          price="$74.00"
+          cutPrice=" "
+        />
+
+        <CarRentalCard
+          name="CR  - V"
+          type="SUV"
+          image1="/08.png"
+          image2="/Spesification7.png"
+          price="$80.00"
+          cutPrice=" "
+        />
+
+        <CarRentalCard
+          name="MG ZX Exclusice"
+          type="Hatchback"
+          image1="/09.png"
+          image2="/Spesification8.png"
+          price="$76.00"
+          cutPrice="$80.00"
+        />
+
+        <CarRentalCard
+          name="New MG ZS"
+          type="SUV"
+          image1="/10.png"
+          image2="/Spesification5.png"
+          price="$80.00"
+          cutPrice=" "
+        />
+
+        <CarRentalCard
+          name="MG ZX Excite"
+          type="Hatchback"
+          image1="/11.png"
+          image2="/Spesification9.png"
+          price="$74.00"
+          cutPrice=" "
+        />
+
+        <CarRentalCard
+          name="New MG ZS"
+          type="SUV"
+          image1="/12.png"
+          image2="/Spesification7.png"
+          price="$80.00"
+          cutPrice=" "
+        />
       </div>
 
       <div className="flex justify-center py-2 sm:py-28">
-        <button className="px-6 py-3 bg-[#3563E9] font-semibold text-white text-16px rounded shadow-md hover:bg-blue-600">
-          Show more car
-        </button>
+        <Link href={"/category"}>
+          <button className="px-6 py-3 font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600">
+            Show more cars
+          </button>
+        </Link>
 
         <p className="absolute right-8 text-[#90A3BF] text-14px font-medium">
           120 Car
