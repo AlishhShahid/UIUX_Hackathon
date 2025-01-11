3df9a0dd72208a89fe6309370ad2ea0a3b3205fb
Alishba_Shahid <165990022+AlishhShahid@users.noreply.github.com>
2025-01-11 20:20:31 +0500

    Update Cars.tsx

--- app/components/Cars.tsx
+++ app/components/Cars.tsx
@@ -1,44 +1,102 @@
 
+import CarRentalCard from "./CarRentalCard";
+
 function Cars() {
-    return (
-      <div className="p-6 bg-gray-100">
-        <div className="grid sm:grid-cols-3 grid-cols-1 gap-4">
-          <div className="rounded-lg overflow-hidden shadow-md">
-            <img src="/Catalog 1.png" alt="Catalog 01" className="w-full h-auto" />
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-md">
-            <img src="/Catalog 2.png" alt="Catalog 02" className="w-full h-auto" />
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-md">
-            <img src="/Catalog 3.png" alt="Catalog 03" className="w-full h-auto" />
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-md">
-            <img src="/1.png" alt="Catalog 01" className="w-full h-auto" />
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-md">
-            <img src="/2.png" alt="Catalog 02" className="w-full h-auto" />
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-md">
-            <img src="/3.png" alt="Catalog 03" className="w-full h-auto" />
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-md">
-            <img src="/5.png" alt="Catalog 05" className="w-full h-auto" />
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-md">
-            <img src="/8.png" alt="Catalog 08" className="w-full h-auto" />
-          </div>
-          <div className="rounded-lg overflow-hidden shadow-md">
-            <img src="/7.png" alt="Catalog 07" className="w-full h-auto" />
-          </div>
-        </div>
-        <div className="flex justify-center py-28">
-          <button className="px-6 py-3 bg-[#3563E9] font-semibold text-white text-16px rounded shadow-md hover:bg-blue-600">
-            Show more car
-          </button>
-          <p className="absolute right-8 text-[#90A3BF] text-14px font-medium">120 Car</p>
-        </div>
+  return (
+    <div className="p-6 bg-gray-100">
+      <div className="grid sm:grid-cols-3 grid-cols-1 gap-4">
+        <CarRentalCard
+          name="Koenigsegg"
+          type="Sport"
+          image1="/01.png"
+          image2="/Spesification1.png"
+          price="$99.00"
+          cutPrice=""
+        />
+
+        <CarRentalCard
+          name="Nissan GT - R"
+          type="Sport"
+          image1="/02.png"
+          image2="/Spesification2.png"
+          price="$80.00"
+          cutPrice="$100.00"
+        />
+
+        <CarRentalCard
+          name="Rolls - Royce"
+          type="Sport"
+          image1="/03.png"
+          image2="/Spesification3.png"
+          price="$96.00"
+          cutPrice=""
+        />
+
+        <CarRentalCard
+          name="All New Rush"
+          type="SUV"
+          image1="/05.png"
+          image2="/Spesification4.png"
+          price="$72.00"
+          cutPrice="$80.00"
+        />
+
+        <CarRentalCard
+          name="CR  - V"
+          type="SUV"
+          image1="/06.png"
+          image2="/Spesification5.png"
+          price="$80.00"
+          cutPrice=" "
+        />
+
+        <CarRentalCard
+          name="All New Terios"
+          type="SUV"
+          image1="/07.png"
+          image2="/Spesification6.png"
+          price="$74.00"
+          cutPrice=" "
+        />
+
+        <CarRentalCard
+          name="MG ZX Exclusice"
+          type="Hatchback"
+          image1="/09.png"
+          image2="/Spesification8.png"
+          price="$76.00"
+          cutPrice="$80.00"
+        />
+
+        <CarRentalCard
+          name="New MG ZS"
+          type="SUV"
+          image1="/10.png"
+          image2="/Spesification5.png"
+          price="$80.00"
+          cutPrice=" "
+        />
+
+        <CarRentalCard
+          name="MG ZX Excite"
+          type="Hatchback"
+          image1="/11.png"
+          image2="/Spesification9.png"
+          price="$74.00"
+          cutPrice=" "
+        />
       </div>
-    );
-  }
-  
-  export default Cars;
+
+      <div className="flex justify-center py-28">
+      <button className="px-6 py-3 font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600">
+          Show more cars
+        </button>
+        <p className="absolute right-8 text-[#90A3BF] text-14px font-medium">
+          120 Car
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Cars;
